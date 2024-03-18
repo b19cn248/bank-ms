4e471be3e66e968df62ddb3b59748960e8821726
Hieu Nguyen <hieunm123.ptit@gmail.com>
2024-03-18 22:42:00 +0700

    refactor code

---
+++ run.sh
@@ -0,0 +1,19 @@
+cd accounts
+mvn compile jib:dockerBuild
+
+cd ..
+cd loans
+mvn compile jib:dockerBuild
+
+cd ..
+cd cards
+mvn compile jib:dockerBuild
+
+cd ..
+cd configserver
+mvn compile jib:dockerBuild
+
+cd ..
+cd docker-compose/default
+
+docker-compose up -d
